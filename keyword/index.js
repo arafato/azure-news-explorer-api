6a5b2ae3d1b22786fcbb33911fa4e6aa814dcd1f
arafato <oliver.arafat@outlook.com>
2016-04-22 10:04:58 +0200

    cleaning up

--- keyword/index.js
+++ keyword/index.js
@@ -1,5 +1,3 @@
-var s = require('../lib/keywordsearch');
-
 module.exports = function (context, req) {
     if (!req.query.keyword) {
         context.res = {
@@ -9,20 +7,8 @@ module.exports = function (context, req) {
         context.done();
     }
 
-    s.keywordSearch(req.query.keyword, function (err, data) {
-        if (err) {
-            context.res = {
-                status: 400, 
-                body: "Failed to query data: " + JSON.stringify(err)
-            }
-            context.done();
-        }
-        
-        context.res = {
-            status: 200,
-            body: JSON.stringify(data)
-        };
-
-        context.done();
-    });
+    context.res = {
+        status: 200,
+        body: req.query.keyword
+    };
 };
