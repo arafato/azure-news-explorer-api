542431423caafd5cc1dc346cb37363822c23b6a8
arafato <oliver.arafat@outlook.com>
2016-04-22 14:22:46 +0200

    implementation of news query

--- news/index.js
+++ news/index.js
@@ -1,3 +1,12 @@
+/* global Buffer */
+var request = require('request');
+var config = require('../lib/config');
+var news = require('../lib/newsquery');
+
+/* 
+ * req.q is is expected to be encoded as base64. We pass through (and thus support) 
+ * the Azure Search query parameters as documented at https://msdn.microsoft.com/en-us/library/azure/dn798927.aspx
+ */
 module.exports = function (context, req) {
     if (!req.query.q) {
         context.res = {
@@ -7,10 +16,21 @@ module.exports = function (context, req) {
         context.done();
     }
 
-    context.res = {
-        status: 200,
-        body: req.query.keyword
-    };
-    
-    context.done();
+    var q = new Buffer("req.q", 'base64').toString('utf8');
+    news.query(q, function (err, data) {
+        if (err) {
+            context.res = {
+                status: 400,
+                body: JSON.stringify(err)
+            };
+            context.done();
+        }
+
+        context.res = {
+            status: 200,
+            body: data
+        };
+        
+        context.done();
+    });
 };
