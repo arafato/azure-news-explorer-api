fe4ca5d29fad5bd2fa95e2211fea8d0d55aa3096
arafato <oliver.arafat@outlook.com>
2016-04-24 20:12:39 +0200

    implementation of keyword query

--- news/index.js
+++ news/index.js
@@ -2,34 +2,46 @@
 var config = require('../lib/config');
 var news = require('../lib/newsquery');
 
+function handleResult(context, err, data) {
+    if (err) {
+        context.res = {
+            status: 400,
+            body: JSON.stringify(err)
+        };
+        context.done();
+    }
+
+    context.res = {
+        status: 200,
+        body: data
+    };
+
+    context.done();
+}
+
 /* 
  * req.q is is expected to be encoded as base64. We pass through (and thus support) 
  * the Azure Search query parameters as documented at https://msdn.microsoft.com/en-us/library/azure/dn798927.aspx
  */
 module.exports = function (context, req) {
-    if (!req.query.q) {
+    if (!req.query.q && !req.query.keyword) {
         context.res = {
             status: 400,
-            body: "Missing query parameter 'q'"
+            body: "Missing query parameter 'q' or 'keyword'"
         };
         context.done();
     }
 
-    var q = new Buffer(req.query.q, 'base64').toString('utf8');
-    news.query(q, function (err, data) {
-        if (err) {
-            context.res = {
-                status: 400,
-                body: JSON.stringify(err)
-            };
-            context.done();
-        }
+    if (req.query.q) {
+        var q = new Buffer(req.query.q, 'base64').toString('utf8');
+        news.rawQuery(q, function (err, data) {
+            handleResult(context, err, data);
+        });
+    }
 
-        context.res = {
-            status: 200,
-            body: data
-        };
-        
-        context.done();
-    });
+    if (req.query.keyword) {
+        news.keywordQuery(req.query.keyword, req.query.ordermode, req.query.startdate, req.query.enddate, function (err, data) {
+            handleResult(context, err, data);
+        })
+    }
 };
